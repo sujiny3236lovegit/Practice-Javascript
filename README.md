d1f3d103133cfe9d40cfe0ad4ad231348afeb5b8
sujiny3236lovegit <69749672+sujiny3236lovegit@users.noreply.github.com>
2020-11-16 19:36:48 +0900

    Javascript: variable

--- README.md
+++ README.md
@@ -35,22 +35,38 @@
       * head + defer: html을 parsing하는 동안 defer옵션을 보면 다운로드 **명령만 해놓은 뒤** 나머지 html을 끝까지 parsing한다. 그리고 parsing이 끝난 다음 다운로드 되어진 js를 실행하게 된다. defer은 html parsing 후 다운로드 받아진 순서대로 실행하므로 내가 정의한 순서에 맞춰서 js가 실행되어 진다. 
 * 순수 바닐라JS를 사용할때면 **'use strict'**작성해 JS의 유연함에 따라오는 위험성을 줄인다.
 * 프로그래밍에서 가장 중요한 것: 1.입력 2.연산 3.출력 
-  * 사용자에게 console이나 UI에서 데이터를 입력받음
-  * 입력받은 데이터를 잘 처리하고 연산함
-  * 알맞게 사용자에게 출력해서 보여줌
-  * ++ 받아온 데이터를 서버로 전송하고 받아오는 것도 중요하다.
+  * 입력: 사용자에게 console이나 UI에서 데이터를 입력받음
+  * 연산: 입력받은 데이터를 잘 처리하고 연산함
+  * 출력: 알맞게 사용자에게 출력해서 보여줌
+  * ++ 받아온 데이터를 서버로 전송하고 받아오는 것도 중요.
+  * ++ CPU에 최적화된 연산도 중요
+  * ++ 메모리의 사용의 최소화도 중요
 * Variable(변수)
   * **var**
     * var hoisting으로 인해 값이 없어도 가장 상위로 선언되어 진다.
     * block scope라는 개념이 존재하지 않는다. (그 어떤 함수에도 접근이 가능해져 버림)
 * Constance(상수)
   * 포인터가 잠겨서 선언함과 동시에 절대로 값을 변경할 수 없다.
-  * Mutable datatype: 변경이 가능함 => let
-  * Immutable datatype: 변경이 불가능함 => const 
+  * Mutable datatype: 값의 변경이 가능함 => let
+  * Immutable datatype: 값의 변경이 불가능함 => const 
     - 보안상의 문제가 발생할 수 있으므로 const를 사용해 프로그래밍을 작성하는 것이 중요하다. 
 * Variable types
-  * primitive, single item(더이상 작은 단위로 나뉠 수 없는 한가지의 아이템)
-    - number, string, boolean, null, undefinend, symble...
-  * object, box container
-  * function, first-class function
-  
+  * **primitive**, single item
+    - 더이상 작은 단위로 나뉠 수 없는 한가지의 아이템 
+    - number, string, boolean, null, undefinend, symble
+  * **object**, box container
+    - 싱글 아이템들을 여러개 묶은, 즉 한 단위 혹은 한 박스
+  * **first-class function**, function
+    - function(함수)도 다른 데이터 타입과 같이 변수에 할당이 가능
+    - 함수의 인자로도 전달이 가능
+    - 함수에서 리턴타입으로도 function이 리턴 가능
+  * number: specialnumeric values: infinity, -infinity, NaN
+  * bigInt: fairly new, dont use it yet
+  * string
+  * boolean
+  * null: 말그대로 아무것도 아닌, 텅텅비어진 
+  * undefined: 변수는 지정되었지만 값이 할당 되어있지 않은
+  * symbol
+
+* Dynamic typing: dynamically typed language
+* TypeScript: 자바스크립트 위에 타입이 더 올려진 언어 
--- variable.js
+++ variable.js
@@ -1,1 +1,112 @@
-console.log("Hello World");
+//1. Use strict 
+// added in ES5
+// use this for Vanila Javascript
+'use strict';
+
+// 2. Varialble 
+// let (added in ES6)
+let globalName = 'global name';
+{
+  let name = 'Alice';
+  console.log(name);
+  name = 'hello';
+  console.log(name);
+  console.log(globalName);
+}
+console.log(name);
+// global 변수들은 application의 시작부터 끝까지 메모리에 탑재되므로 최소한으로만 사용하고,
+// 가능하면 class, function(함수), if, for loop을 사용해서 정의하는 것이 좋다.
+
+// 2-2. var (dont ever use this)
+// var hoisting: hoisting은 어디에 선언했느냐에 상관없이 최상단으로 끌어올려주는 것
+// var has no block scope
+console.log(age);
+age = 4;
+var age;
+
+name = 4;
+let name;
+
+// 3. Constants
+// favor immutable data type always for a reasons: 한번 할당하면 값을 절대 변경할 수 없다.
+//  - security
+//  - thread safety
+//  - reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// 4. Variable types
+//  - primitive, single item: number, string, boolean, null, undefinend, symble
+//  - object, box container
+//  - function, first-class function 
+const count = 17; //integer
+const size = 17.1; //decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+//  - object, real-life object, data structue
+const ellie = { name: 'sujin', age: 30 }
+sujin.age = 30;
+//  - number - specialnumeric values: infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+//  - bigInt (fairly new, dont use it yet);
+const bigInt = 1234567890123456789012345678901234567890n; //over (-2**53) ~ 2* 53)
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+Number.MAX_SAFE_INTEGER;
+
+//  - string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!` //template literals (string);
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
+//  - boolean
+//     - false: 0, null, undefined, NaN, '' 
+//     - true: any other value 
+const canRead = true;
+const test = 3 < 1; //false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+//  - null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+//  - undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+//  - symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(tymbol1 === symbol2);
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); // true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'h';
+console.log(text.charAt(0)); //h
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
+console.log(text.charAt(0));
+
+// 6. 
+// 
+// 
+// 
+
