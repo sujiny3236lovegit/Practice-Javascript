ea35cd00c3d11611ed2112383d1deb82f8051890
sujiny3236lovegit <69749672+sujiny3236lovegit@users.noreply.github.com>
2021-01-06 22:54:39 +0900

    css+javascript practice

--- Elie/chapter8/teacher/main.js
+++ Elie/chapter8/teacher/main.js
@@ -12,9 +12,9 @@ const gameScore = document.querySelector('.game__score');
 
 let started = false;
 let score = 0;
-let timer = undefined;
+let timer = undefined; 
 
-gameBtn.addEventListener('click', () =>{
+gameBtn.addEventListener('click', ()=>{
   if(started){
     stopGame();
   }else{
@@ -26,39 +26,24 @@ gameBtn.addEventListener('click', () =>{
 function startGame(){
   initGame();
   showStopButton();
-  showTimerAndScore();
-  startGameTimer();
-};
-
-function stopGame(){
-
-};
-
-function showStopButton(){
-  const icon = gameBtn.querySelector('.fa-play');
-  icon.classList.add('fa-stop');
-  icon.classList.remove('fa-play');
 }
 
-function showTimerAndScore(){
-  gameTimer.style.visibility = 'visible';
-  gameScore.style.visibility = 'visible';
+function stopGame(){
 }
 
 function initGame(){
-  field.innerHTML = '';
-  gameScore.innerText = CARROT_COUNT;
+  // 벌레와 당근을 생성한뒤에 field에 추가해준다.
   addItem('carrot', CARROT_COUNT, 'img/carrot.png');
   addItem('bug', BUG_COUNT, 'img/bug.png');
 }
 
-function addItem(className, count, imgPath){
+function addItem(className, count, imgPath){ 
   const x1 = 0;
   const y1 = 0;
   const x2 = fieldRect.width - CARROT_SIZE;
   const y2 = fieldRect.height - CARROT_SIZE;
   for(let i = 0; i < count; i++){
-    const item = document.createElement('img');
+    let item = document.createElement('img');
     item.setAttribute('class', className);
     item.setAttribute('src', imgPath);
     item.style.position = 'absolute';
@@ -67,12 +52,11 @@ function addItem(className, count, imgPath){
     item.style.left = `${x}px`;
     item.style.top = `${y}px`;
     field.appendChild(item);
-  }
-}
+  };
+};
 
-function randomNumber(min, max) { 
-  return Math.floor(Math.random() * (max - min) + min);
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
 }
-
 
 initGame();
